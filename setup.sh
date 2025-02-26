7a4ea6f67316cf36331f30b5e1bab150312d1cb6
Rob Clucas <robjclucas@gmail.com>
2025-02-26 17:52:29 +0000

    Update startup scripts

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y build-essential
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source $HOME/.cargo.env
+rustup toolchain install nightly
+rustup default nightly
+rustup component add rust-analyzer
